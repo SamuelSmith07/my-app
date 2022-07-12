1d1d7ad9ee492e4d98487c1817ba1987db0d9d0d
SamuelSmith07 <samsmith1@live.com>
2022-07-12 18:12:31 +1000

    begining of unity app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import Unity_Technologies_logo from './Unity_Technologies_logo.svg';
 import './App.css';
+import './Data.js';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <img src={Unity_Technologies_logo} className="App-logo" alt="Unity Logo" />
+        <h1>Recipe Book</h1>
+        <Box
+      sx={{
+        position: absolute,
+        width: 1080,
+        height: 720,
+        left: 426,
+        top: 192,
+        
+        backgroundColor: '#2D2D2D',
+        border-radius: '45';
+      }}
+    />
+        <Button variant="contained">Hello World</Button>
       </header>
     </div>
   );
---
+++ src/Data.js
@@ -0,0 +1,15 @@
+import React from "react";
+import gun from './gun.png';
+import chess from './chess.png';
+
+const BtnData = () =>{
+
+const Data = [
+    {id: 1, name: "FPS", image: {gun}},
+    {id: 2, name: "RTS", image: {chess}}
+
+
+
+]
+
+}
